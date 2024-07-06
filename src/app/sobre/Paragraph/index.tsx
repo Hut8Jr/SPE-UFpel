532422d804c16bd2e783d24b4209a176b9ef3441
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2024-07-06 14:02:24 -0300

    About FIX DESKTOP

--- src/app/sobre/Paragraph/index.tsx
+++ src/app/sobre/Paragraph/index.tsx
@@ -7,13 +7,15 @@ export function Paragraph() {
       <p className="mt-[60px] text-[27px] font-light leading-[43px] text-black">
         O Capítulo Estudantil SPE da Universidade Federal de Pelotas foi fundado
         no dia 05 de julho de 2012, pelos alunos de graduação em Engenharia de
-        Petróleo. <br />
-        <br />O Capítulo conta atualmente com uma diretoria que têm como
-        objetivo aproximar a cadeia produtiva de petróleo, localizada
-        principalmente no Sudeste Brasileiro, à nossa Universidade (UFPEL). Essa
-        aproximação é feita através de palestras, cursos de curta duração com
-        profissionais experientes na área, visitas técnicas e também na Semana
-        Acadêmica realizada anualmente, o Petro-Sul.
+        Petróleo.
+        <br />
+        <br />
+        O Capítulo conta atualmente com uma diretoria que têm como objetivo
+        aproximar a cadeia produtiva de petróleo, localizada principalmente no
+        Sudeste Brasileiro, à nossa Universidade (UFPEL). Essa aproximação é
+        feita através de palestras, cursos de curta duração com profissionais
+        experientes na área, visitas técnicas e também na Semana Acadêmica
+        realizada anualmente, o Petro-Sul.
         <br />
         <br />
         Além disso, o Capítulo preza pela responsabilidade social, buscando
