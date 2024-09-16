336442fd96c9e95711dbf07a7d38ce9a0dcfdb9e
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2024-09-16 19:57:32 -0300

    Project page MOBILE

--- src/components/Project/AboutProject/index.tsx
+++ src/components/Project/AboutProject/index.tsx
@@ -1,11 +1,11 @@
 export function AboutProject() {
   return (
-    <div className="container py-[117px]">
+    <div className="container py-[40px] lg:py-[117px]">
       <div className="max-w-[1112px]">
-        <h1 className="mb-[59px] text-[70px] font-light leading-[74px] text-black">
+        <h1 className="mb-[20px] text-[36px] font-light leading-[36px] text-black lg:mb-[59px] lg:text-[70px] lg:leading-[74px]">
           Speaking
         </h1>
-        <p className="text-[27px] leading-[43px] text-black">
+        <p className="text-lg text-black lg:text-[27px] lg:leading-[43px]">
           Com o objetivo de juntar a cultura da língua inglesa e o curso de
           Engenharia de Petróleo, o speaking foi criado com o intuito de
           despertar o interesse dos estudantes e ao mesmo tempo aprimorar o
--- src/components/Project/Hero/index.tsx
+++ src/components/Project/Hero/index.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export function Hero() {
   return (
-    <section className="container bg-white">
+    <section className="container bg-brand-dark-blue">
       <Image
         src="/projeto1.png"
         height={680}
         width={1350}
         alt="Projeto 1"
-        className="max-h-[680px] max-w-[1350px]"
+        className="aspect-video"
         style={{
           objectFit: 'fill',
         }}
