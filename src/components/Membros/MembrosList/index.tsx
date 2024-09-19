48f0a1cdbbe71db7d5db4af69b03ef98a3f097dd
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2024-09-19 03:42:56 -0300

    Membros Pagina

--- src/components/Membros/MembrosList/index.tsx
+++ src/components/Membros/MembrosList/index.tsx
@@ -1,9 +1,4 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@/components/ui/accordion'
+import { Accordion } from '@/components/ui/accordion'
 import { Membro } from './Membro'
 
 export function MembrosList() {
